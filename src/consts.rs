4fd00d839bd8bac18f4cf7403d70e12defdc5fa9
Gabriel Dos Santos <gabriel.dossantos@cea.fr>
2023-04-20 15:42:27 +0200

    Added crate-level constants

---
+++ src/consts.rs
@@ -0,0 +1,13 @@
+//! Crate-level constants.
+
+/// Default number of meta repetitions performed when benchmarking a kernel.
+pub const META_REPETITIONS: u8 = 31;
+
+/// Default number of tight loop repetitions performed when benchmarking a kernel.
+pub const TIGHT_LOOP_REPETITIONS: u16 = 1;
+
+/// Vector block size.
+pub const BLOCK_SIZE_1D: usize = 1024;
+
+/// Matrix block size.
+pub const BLOCK_SIZE_2D: usize = 32;
