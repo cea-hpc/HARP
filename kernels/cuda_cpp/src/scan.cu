1da19f9857d06d0bc602a9e4d99561b2cffd61a4
Gabriel Dos Santos <gabriel.dossantos@cea.fr>
2023-08-31 16:14:18 +0200

    Updated kernels

--- kernels/cuda_cpp/src/scan.cu
+++ kernels/cuda_cpp/src/scan.cu
@@ -1,121 +1,133 @@
-#include <cstdint>
+#include <stdint.h>
 
-/// Logarithm of number of memory banks (assuming 32).
-constexpr size_t LOG_NB_BANKS = 5;
+#define MAX_BLOCK_SZ 1024
+#define NUM_BANKS 32
+#define LOG_NUM_BANKS 5
 
-/// Update each element of the output buffer by adding the partial sum of the previous block.
+#ifdef ZERO_BANK_CONFLICTS
+    #define CONFLICT_FREE_OFFSET(n) ((n) >> NUM_BANKS + (n) >> (2 * LOG_NUM_BANKS))
+#else
+    #define CONFLICT_FREE_OFFSET(n) ((n) >> LOG_NUM_BANKS)
+#endif
+
 extern "C" __global__
-auto add_block_sums(
-    int32_t const* d_in,
-    size_t len,
+void device_add_block_sums(
     int32_t* d_out,
-    int32_t const* block_sums,
-    size_t _len2
-) -> void {
-    size_t idx = 2 * blockIdx.x * blockDim.x + threadIdx.x;
+    int32_t const* d_in,
+    int32_t* d_block_sums,
+    size_t numElems
+) {
+    int32_t d_block_sum_val = d_block_sums[blockIdx.x];
 
-    int32_t block_sum = block_sums[blockIdx.x];
-    if (idx < len) {
-        d_out[idx] = d_in[idx] + block_sum;
-        if (idx + blockDim.x < len) {
-            d_out[idx + blockDim.x] = d_in[idx + blockDim.x] + block_sum;
+    // Simple implementation's performance is not significantly (if at all)
+    // better than previous verbose implementation
+    size_t cpy_idx = 2 * blockIdx.x * blockDim.x + threadIdx.x;
+    if (cpy_idx < numElems) {
+        d_out[cpy_idx] = d_in[cpy_idx] + d_block_sum_val;
+        if (cpy_idx + blockDim.x < numElems) {
+            d_out[cpy_idx + blockDim.x] = d_in[cpy_idx + blockDim.x] + d_block_sum_val;
         }
     }
 }
 
-/// Modified version of Mark Harris' implementation of the Blelloch scan according to 
-/// `https://www.mimuw.edu.pl/~ps209291/kgkp/slides/scan.pdf`.
+// Modified version of Mark Harris' implementation of the Blelloch scan
+//  according to https://www.mimuw.edu.pl/~ps209291/kgkp/slides/scan.pdf
 extern "C" __global__
-auto scan(
-    int32_t const* d_in,
-    size_t len,
+void device_scan(
     int32_t* d_out,
-    int32_t* block_sums,
-    size_t block_size,
-    size_t smem_size
-) -> void {
+    int32_t const* d_in,
+    int32_t* d_block_sums,
+    size_t len,
+    size_t shmem_sz,
+    size_t max_elems_per_block
+) {
     // Allocated on invocation
-    extern __shared__ int32_t sdata[];
+    extern __shared__ int32_t s_out[];
 
-    size_t tid = threadIdx.x;
-    size_t ai = tid;
-    size_t bi = tid + blockDim.x;
+    size_t thid = threadIdx.x;
+    size_t ai = thid;
+    size_t bi = thid + blockDim.x;
 
-    // Zero out the shared memory (especially helpful when input size is not power of two)
-    sdata[ai] = 0;
-    sdata[bi] = 0;
-    if (tid + block_size < smem_size) {
-        sdata[tid + block_size] = 0;
+    // Zero out the shared memory
+    // Helpful especially when input size is not power of two
+    s_out[thid] = 0;
+    s_out[thid + blockDim.x] = 0;
+    // If CONFLICT_FREE_OFFSET is used, shared memory
+    // must be a few more than 2 * blockDim.x
+    if (thid + max_elems_per_block < shmem_sz) {
+        s_out[thid + max_elems_per_block] = 0;
     }
+
     __syncthreads();
 
-    // Copy `d_in` to shared memory
-    // NOTE: the input elements are scattered into shared memory in light of avoiding bank conflicts.
-    size_t idx = threadIdx.x + block_size * blockIdx.x;
-    if (idx < len) {
-        sdata[ai + (ai >> LOG_NB_BANKS)] = d_in[idx];
-        if (idx + blockDim.x < len) {
-            sdata[bi + (bi >> LOG_NB_BANKS)] = d_in[idx + blockDim.x];
+    // Copy d_in to shared memory
+    // Note that d_in's elements are scattered into shared memory
+    // in light of avoiding bank conflicts
+    size_t cpy_idx = max_elems_per_block * blockIdx.x + threadIdx.x;
+    if (cpy_idx < len) {
+        s_out[ai + CONFLICT_FREE_OFFSET(ai)] = d_in[cpy_idx];
+        if (cpy_idx + blockDim.x < len) {
+            s_out[bi + CONFLICT_FREE_OFFSET(bi)] = d_in[cpy_idx + blockDim.x];
         }
     }
 
     // For both upsweep and downsweep:
-    // Sequential indices with conflict free padding.
-    // Amount of padding: target index / number of banks
-    // This "shifts" the target indices by one every multiple of the number of banks.
-    //
-    // `offset` controls the stride and starting index of the target element at every iteration.
-    // `mask` controls which threads are active.
-    // Sweeps are pivoted on the last element of shared memory.
+    // Sequential indices with conflict free padding
+    // Amount of padding = target index / num banks
+    // This "shifts" the target indices by one every multiple
+    // of the num banks
+    // offset controls the stride and starting index of
+    // target elems at every iteration
+    // d just controls which threads are active
+    // Sweeps are pivoted on the last element of shared memory
 
-    // Upsweep/reduce step
+    // Upsweep/Reduce step
     size_t offset = 1;
-    size_t mask = block_size >> 1;
-    while (mask > 0) {
+    for (size_t d = max_elems_per_block >> 1; d > 0; d >>= 1) {
         __syncthreads();
-        if (tid < mask) {
-            size_t ai = offset * ((tid << 1) + 1) - 1;
-            size_t bi = offset * ((tid << 1) + 2) - 1;
-            ai += (ai >> LOG_NB_BANKS);
-            bi += (bi >> LOG_NB_BANKS);
-            sdata[bi] += sdata[ai];
+
+        if (thid < d) {
+            size_t ai = offset * ((thid << 1) + 1) - 1;
+            size_t bi = offset * ((thid << 1) + 2) - 1;
+            ai += CONFLICT_FREE_OFFSET(ai);
+            bi += CONFLICT_FREE_OFFSET(bi);
+
+            s_out[bi] += s_out[ai];
         }
         offset <<= 1;
-        mask >>= 1;
     }
 
-    // Save the total sum on the global block sums array and clear the last element in shared memory
-    if (tid == 0) {
-        block_sums[blockIdx.x] = sdata[block_size - 1 + ((block_size - 1) >> LOG_NB_BANKS)];
-        sdata[block_size - 1 + ((block_size - 1) >> LOG_NB_BANKS)] = 0;
+    // Save the total sum on the global block sums array
+    // Then clear the last element on the shared memory
+    if (thid == 0) {
+        d_block_sums[blockIdx.x] =
+            s_out[max_elems_per_block - 1 + CONFLICT_FREE_OFFSET(max_elems_per_block - 1)];
+        s_out[max_elems_per_block - 1 + CONFLICT_FREE_OFFSET(max_elems_per_block - 1)] = 0;
     }
 
     // Downsweep step
-    mask = 1;
-    offset >>= 1;
-    while (mask < block_size) {
-        __syncthreads();
-        if (tid < mask) {
-            size_t ai = offset * ((tid << 1) + 1) - 1;
-            size_t bi = offset * ((tid << 1) + 2) - 1;
-            ai += (ai >> LOG_NB_BANKS);
-            bi += (bi >> LOG_NB_BANKS);
-
-            int32_t tmp = sdata[ai];
-            sdata[ai] = sdata[bi];
-            sdata[bi] += tmp;
-        }
-
-        mask <<= 1;
+    for (size_t d = 1; d < max_elems_per_block; d <<= 1) {
         offset >>= 1;
+        __syncthreads();
+
+        if (thid < d) {
+            size_t ai = offset * ((thid << 1) + 1) - 1;
+            size_t bi = offset * ((thid << 1) + 2) - 1;
+            ai += CONFLICT_FREE_OFFSET(ai);
+            bi += CONFLICT_FREE_OFFSET(bi);
+
+            int32_t tmp = s_out[ai];
+            s_out[ai] = s_out[bi];
+            s_out[bi] += tmp;
+        }
     }
     __syncthreads();
 
     // Copy contents of shared memory to global memory
-    if (idx < len) {
-        d_out[idx] = sdata[ai + (ai >> LOG_NB_BANKS)];
-        if (idx + blockDim.x < len) {
-            d_out[idx + blockDim.x] = sdata[bi + (bi >> LOG_NB_BANKS)];
+    if (cpy_idx < len) {
+        d_out[cpy_idx] = s_out[ai + CONFLICT_FREE_OFFSET(ai)];
+        if (cpy_idx + blockDim.x < len) {
+            d_out[cpy_idx + blockDim.x] = s_out[bi + CONFLICT_FREE_OFFSET(bi)];
         }
     }
 }
